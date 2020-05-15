099aa55a462be1dce2a79de159a896c6abd1a980
SrinathMurali96 <rockinsrinath96@gmail.com>
2020-05-15 17:28:01 +0530

    Update Longest_Palindrome.py

--- Longest_Palindrome.py
+++ Longest_Palindrome.py
@@ -8,3 +8,13 @@ for i in range(0,len(a)+1):
                 if len(long_palindrome)<len(val):
                     long_palindrome = val
 print("long_palindrome is ",long_palindrome)
+
+
+for i in range(0,len(a)):
+    sum1 = ''
+    for j in range(0,len(a)):
+        sum1 = sum1 + a[j]
+        if sum1 == sum1[::-1]:
+            if len(long_palindrome)<len(val):
+                    long_palindrome = val
+print("long_palindrome is ",long_palindrome)
